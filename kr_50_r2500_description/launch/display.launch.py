6fa94b747731e3cd243272ceb03726fd34ff8fab
MichiDelle <michi.delle07@gmail.com>
2023-10-19 15:41:13 +0200

    rsi tests

---
+++ kr_50_r2500_description/launch/display.launch.py
@@ -0,0 +1,135 @@
+# Copyright 2023 Áron Svastits
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+
+from ament_index_python import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, OpaqueFunction
+from launch.actions import IncludeLaunchDescription
+from launch.actions import GroupAction
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution, FindExecutable
+from launch.substitutions import Command, TextSubstitution
+from launch_ros.actions import Node
+from launch_ros.actions import PushRosNamespace
+from launch_ros.substitutions import FindPackageShare
+from launch_ros.parameter_descriptions import ParameterValue
+
+
+def launch_setup(context, *args, **kwargs):
+
+    # args that can be set from the command line or a default will be used
+    prefix = LaunchConfiguration("prefix")
+    robot_name = LaunchConfiguration("robot_name")
+    simulation = LaunchConfiguration('simulation')
+
+    rviz_config_file = PathJoinSubstitution([FindPackageShare('kr_50_r2500_description'),
+                                            "config", "view_cell.rviz"]
+                                            )
+
+    robot_description_content = ParameterValue(
+        Command(
+            [
+                PathJoinSubstitution([FindExecutable(name='xacro')]),
+                " ",
+                PathJoinSubstitution(
+                    [FindPackageShare('kr_50_r2500_description'),
+                     "urdf", "kr_50_r2500_rviz.xacro"]
+                ),
+                " ",
+                "robot_name:=",
+                robot_name.perform(context),
+                " ",
+                "prefix:=",
+                prefix.perform(context),
+            ]
+        ),
+        value_type=str
+    )
+
+    print(type(robot_description_content))
+    robot_description = {'robot_description': robot_description_content}
+
+    if (simulation.perform(context) == "True"):
+        joint_state_publisher = Node(
+            name='joint_state_publisher',
+            package='joint_state_publisher',
+            executable='joint_state_publisher',
+            parameters=[{'use_gui': False},
+                        {'rate': 500},
+                        {'source_list': ['/kr_50_r2500/joint_states']}]
+        )
+
+    if (simulation.perform(context) == "False"):
+        joint_target_publisher = Node(
+            name='joint_target_publisher',
+            package='joint_state_publisher_gui',
+            executable='joint_state_publisher_gui',
+            parameters=[{'rate': 500}],
+        )
+    elif (simulation.perform(context) == "True"):
+        joint_target_publisher = Node(
+            name='joint_target_publisher',
+            package='joint_state_publisher_gui',
+            executable='joint_state_publisher_gui',
+            parameters=[{'rate': 500}],
+            remappings=[('/joint_states', '/kr_50_r2500/joint_target'),]
+        )
+
+    robot_state_publisher = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        parameters=[robot_description],
+    )
+
+    rviz = Node(
+        package="rviz2",
+        executable="rviz2",
+        name="rviz2",
+        arguments=["-d", rviz_config_file],
+    )
+
+    if (simulation.perform(context) == "True"):
+        nodes_to_start = [
+            joint_state_publisher,
+            joint_target_publisher,
+            robot_state_publisher,
+            rviz]
+    else:
+        nodes_to_start = [
+            joint_target_publisher,
+            robot_state_publisher,
+            rviz]
+
+    return nodes_to_start
+
+
+def generate_launch_description():
+    launch_arguments = []
+    launch_arguments.append(DeclareLaunchArgument(
+        'prefix',
+        default_value=''
+    ))
+    launch_arguments.append(DeclareLaunchArgument(
+        'robot_name',
+        default_value="kr_50_r2500"
+    ))
+    launch_arguments.append(DeclareLaunchArgument(
+        'simulation',
+        default_value="False"
+    ))
+    return LaunchDescription(launch_arguments +
+                             [OpaqueFunction(function=launch_setup)])
---
+++ kr_50_r2500_description/launch/start.launch.py
@@ -0,0 +1,164 @@
+# Copyright 2023 Áron Svastits
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, OpaqueFunction
+from launch.actions import RegisterEventHandler
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+from launch.substitutions import Command, FindExecutable
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+from launch_ros.parameter_descriptions import ParameterValue
+from launch.conditions import IfCondition
+from launch.event_handlers import OnProcessExit
+
+
+def launch_setup(context, *args, **kwargs):
+
+    # args that can be set from the command line or a default will be used
+    prefix = LaunchConfiguration("prefix")
+    robot_name = LaunchConfiguration("robot_name")
+    fake = LaunchConfiguration('fake')
+    rviz_gui = LaunchConfiguration('rviz_gui')
+
+    robot_description = {
+        'robot_description':
+        ParameterValue(
+           Command(
+               [PathJoinSubstitution([FindExecutable(name='xacro')]),
+                " ",
+                PathJoinSubstitution(
+                    [FindPackageShare('kr_50_r2500_description'),
+                     "urdf",
+                     "kr_50_r2500_rviz.xacro"]
+                ),
+                " ",
+                "robot_name:=",
+                robot_name.perform(context),
+                " ",
+                "prefix:=",
+                prefix.perform(context),
+                " ",
+                "use_fake_hardware:=",
+                fake.perform(context),]
+           ),
+           value_type=str
+        )
+    }
+
+    controller_config = PathJoinSubstitution(
+        [FindPackageShare("kr_50_r2500_description"),
+         "config",
+         "ros2_controller_config.yaml"]
+    )
+
+    joint_traj_controller_config = PathJoinSubstitution(
+        [FindPackageShare("kr_50_r2500_description"),
+         "config",
+         "joint_trajectory_controller_config.yaml"]
+    )
+    rviz_config_file = PathJoinSubstitution(
+        [FindPackageShare("kr_50_r2500_description"),
+         "config",
+         "view_cell.rviz"]
+    )
+
+    control_node = Node(
+        package="controller_manager",
+        executable="ros2_control_node",
+        parameters=[robot_description, controller_config],
+        output="both",
+    )
+
+    robot_state_pub_node = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        output="both",
+        parameters=[robot_description],
+    )
+
+    rviz_node = Node(
+        package="rviz2",
+        executable="rviz2",
+        name="rviz2",
+        output="log",
+        arguments=["-d", rviz_config_file],
+        condition=IfCondition(rviz_gui),
+    )
+
+    joint_state_broadcaster_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["joint_state_broadcaster",
+                   "--controller-manager",
+                   "/controller_manager"],
+    )
+    joint_trajectory_controller_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["joint_trajectory_controller",
+                   "-c",
+                   "/controller_manager",
+                   "-p",
+                   joint_traj_controller_config],
+    )
+
+    delay_joint_nodes_after_control_node = RegisterEventHandler(
+            event_handler=OnProcessExit(
+                target_action=control_node,
+                on_exit=[joint_state_broadcaster_spawner,
+                         joint_trajectory_controller_spawner],
+            )
+        )
+
+    joint_target_publisher = Node(
+        name='joint_target_publisher',
+        package='joint_state_publisher_gui',
+        executable='joint_state_publisher_gui',
+        parameters=[{'rate': 500}],
+    )
+
+    nodes_to_start = [
+#        joint_target_publisher,
+        control_node,
+        robot_state_pub_node,
+        rviz_node,
+#        joint_state_broadcaster_spawner,
+#        joint_trajectory_controller_spawner
+        delay_joint_nodes_after_control_node
+        ]
+
+    return nodes_to_start
+
+
+def generate_launch_description():
+    launch_arguments = []
+    launch_arguments.append(DeclareLaunchArgument(
+        'prefix',
+        default_value=''
+    ))
+    launch_arguments.append(DeclareLaunchArgument(
+        'robot_name',
+        default_value="kr_50_r2500"
+    ))
+    launch_arguments.append(DeclareLaunchArgument(
+        'fake',
+        default_value="false"
+    ))
+    launch_arguments.append(DeclareLaunchArgument(
+        'rviz_gui',
+        default_value="true"
+    ))
+    return LaunchDescription(launch_arguments +
+                             [OpaqueFunction(function=launch_setup)])
